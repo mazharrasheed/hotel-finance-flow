860eb0eba053716987053ec867d5298a8be07496
MAZHAR <mazharopal@gmail.com>
2026-01-04 14:58:08 +0500

    change in settings.py

--- hotelfinancemanager/settings.py
+++ hotelfinancemanager/settings.py
@@ -44,8 +44,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
     'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -156,11 +156,9 @@ REST_FRAMEWORK = {
 }
 
 
-
-
-
-
 CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",  # React's dev server
